5c54bc3e28b0998675c44d5927eee27905664bd9
Joseph Sapp <sapp.joey@tamu.edu>
2018-01-28 13:34:10 -0600

    even more updates

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -25,7 +25,7 @@ function movePlan(){
 	// console.log(this.elt);
 	select('#'.concat(this.elt.id)).position(mouseX-60, height-80);
 	console.log(this.elt.id);
-	timeline.hour_idx = this.elt.id.split("-")[1];
+	timeline.hour_idx = (this.elt.id.split("-")[1]).parseInt();
 }
 
 function windowResized() {
--- scripts/timelines.js
+++ scripts/timelines.js
@@ -22,17 +22,24 @@ function Timeline() {
 		var step;
 		stroke("#666666");
 		strokeWeight(3);
+		textSize(10);
+		fill(0, 80);
 		for (step = 1; step < 24; step++) {
+			strokeWeight(1);
+			stroke(0, 80);
+			text(step, step*1/24*this.w - 5, this.h/2 - 3);
+			strokeWeight(3);
+			stroke(0, 120);
 			point(step*1/24*this.w, this.h/2 + 5);
 		}
 
-		if (show_hours && this.hours_idx != -1){
+		if (show_hours && this.hours_idx != -1 && this.hours.length > 0){
 			console.log("hi");
 			console.log(this.hours[this.hour_idx]);
 			var x = 25;
 
-			stroke(0);
-			strokeWeight(5);
+			stroke(20, 240, 20, 80);
+			strokeWeight(4);
 			for (step = 1; step < 23; step++) {
 
 				line(x, this.h - 25 + map(this.hours[this.hour_idx][constrain(step-1, 0, 24)], 0, 100, 0, -140), x + width/24, this.h - 25 + map(this.hours[this.hour_idx][step], 0, 100, 0, -140));
