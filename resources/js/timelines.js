bbd533b468ac46305b76504c7cef234dc8b3e861
Joseph Sapp <sapp.joey@tamu.edu>
2018-02-01 12:39:32 -0600

    Timeline is working as a div

--- resources/js/timelines.js
+++ resources/js/timelines.js
@@ -1,13 +1,14 @@
 // Remember plans = [ ... ]
 function Timeline(t_x, t_y) {
-	this.c = createCanvas(windowWidth, windowHeight/4);
+	this.c = createCanvas(windowWidth, windowHeight*0.2);
 	this.w = t_x;
 	this.h = t_y;
 	this.hours = [];
 	this.hour_idx = -1;
 	this.c.parent('timeline-holder');
 	this.c.id('timeline-div');
-	this.c.style('inline-block');
+	// this.c.style("height", "100%");
+	this.c.style("z-index", "0");
 
 
 	this.c.mouseClicked(function(e){
@@ -26,18 +27,30 @@ function Timeline(t_x, t_y) {
 	}
 
 	this.placePlan = function(x, id, name){
-		var tmp = createDiv(0, 0);
+		var tmp = createDiv(name);
 		tmp.id(id);
-		tmp.html(name);
 		tmp.mouseClicked(this.movePlan);
 		tmp.parent('timeline-holder');	
 
+		tmp.style("top", "0px");
+		tmp.style("position", "absolute");
+
 		tmp.style('background-color', 'white');
-		tmp.style('height', '100%');
-		tmp.style('width', '15%');
-		tmp.style('padding', '0.5em');
-		tmp.style('margin', '2em');
-		tmp.style('display', 'inline-block');
+		// tmp.style('top', '0px');
+		// tmp.style('position', 'absolute');
+		// tmp.style('float', 'left');
+		tmp.style('z-index', '6');
+		// tmp.center();
+
+		console.log(tmp.elt);
+
+
+		tmp.style('padding', '5%');
+		tmp.style('margin', '5%');
+		tmp.style('border-style', 'solid');
+		tmp.style('border-color', '2px 2px black');
+		tmp.style('box-shadow', '3px 3px black');
+
 
 		// jQuery events
 		$('#'+id).draggable({
@@ -50,45 +63,8 @@ function Timeline(t_x, t_y) {
 
 
 	this.display = function(){
-
-		// Base layer
-
+		rect(0, 0, width-1, height-1);
+	
 		
-		// Begin the base for timeline!
-
-		stroke("#e6e6e6");
-		strokeWeight(1.1);
-		fill(255);
-		rect(20, 20, this.w-40, this.h-40);
-
-		var step;
-		stroke("#666666");
-		strokeWeight(3);
-		textSize(10);
-		fill(0, 80);
-		for (step = 1; step < 24; step++) {
-			strokeWeight(1);
-			stroke(0, 80);
-			text(step, step*1/24*this.w - 5, this.h/2 - 3);
-			strokeWeight(3);
-			stroke(0, 120);
-			point(step*1/24*this.w, this.h/2 + 5);
-		}
-
-		if (show_hours && this.hours_idx != -1 && this.hours.length > 0){
-			var x = 25;
-
-			stroke(20, 240, 20, 150);
-			strokeWeight(4);
-			if (this.hours == []){
-				return;
-			}
-			for (step = 1; step < 23; step++) {
-				line(x, this.h - 25 + map(this.hours[this.hour_idx][constrain(step-1, 0, 24)], 0, 100, 0, -120), x + width/24, this.h - 25 + map(this.hours[this.hour_idx][step], 0, 100, 0, -120));
-				x += width/24;
-
-
-			}	
-		}
 	}
 };
